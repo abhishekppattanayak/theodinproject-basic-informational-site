28e8fba035961f6d2ee66c33143aa00a0013a0ca
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2024-06-27 23:58:09 +0530

    V0.1 complete

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 8080
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'))
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'error404.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
